ef1fae538669638285495e3d1328ba77b2bd60c1
LucasTL <lucasliuhaolin@gmail.com>
2024-01-18 16:09:44 +0100

    feat: add navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+const navigation = [
+  { name: "Home", href: "#", current: true },
+  { name: "People", href: "#", current: false },
+  { name: "Project", href: "#", current: false },
+  { name: "Safety", href: "#", current: false },
+  { name: "Practices", href: "#", current: false },
+  { name: "Education", href: "#", current: false },
+];
+
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between bg-black p-3 ">
+      <div>
+        <h1>iGEM LeRosey</h1>
+      </div>
+      <div>
+        {navigation.map((item) => (
+          <Link href={item.href} className="p-2 m-2">
+            {item.name}
+          </Link>
+        ))}
+      </div>
+    </nav>
+  );
+}
